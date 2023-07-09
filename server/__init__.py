43e0c92d08597fc2e5b3e6c52c16f3bd10cb43da
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-07-09 02:26:21 +0000

    instancia api

--- server/__init__.py
+++ server/__init__.py
@@ -7,11 +7,13 @@ import click
 class Servidor(Flask):
     """Classe de inicialização do servidor"""
     init_db = import_module('server.database').init_app
+    init_api = import_module('server.api').init_app
 
     def __init__(self):
         super().__init__(__name__)
         self.config.from_prefixed_env()
         self.db = None  # pylint: disable=invalid-name
+        self.api = None  # pylint: disable=invalid-name
 
     def create_database(self):
         """Cria o banco de dados"""
@@ -30,4 +32,5 @@ class Servidor(Flask):
         """Inicializa o servidor"""
         with self.app_context():
             self.db = self.init_db()
+            self.api = self.init_api()
             self.commands()
