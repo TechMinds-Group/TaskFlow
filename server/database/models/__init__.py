7809d1a9a1f2bca6eb9c2699c68c3372d6b80a4f
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-07-09 05:39:04 +0000

    importando os models

--- server/database/models/__init__.py
+++ server/database/models/__init__.py
@@ -1,4 +1,8 @@
 """Modulo de modelos do banco de dados"""
 from .usuario import Usuario
+from .espaco import Espaco
+from .espaco_de_trabalho import EspacoDeTrabalho
+from .membro import Membro
+from .membro_espaco import MembroEspaco
 
-__all__ = ['Usuario']
+__all__ = ['Usuario', 'Espaco', 'EspacoDeTrabalho', 'Membro', 'MembroEspaco']
