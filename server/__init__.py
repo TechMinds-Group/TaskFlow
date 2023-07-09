4fce52a5abab2cafa207821b8f40be4bf2c43ddf
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-07-09 01:00:38 +0000

    criação do banco

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from importlib import import_module
+import click
 
 
 class Servidor(Flask):
@@ -9,5 +10,18 @@ class Servidor(Flask):
         super().__init__(__name__)
         self.config.from_prefixed_env()
 
+    
+    def create_database(self):
+        """Cria o banco de dados"""
+        with self.app_context():
+            self.db.create_all()
+
+    def commands(self):
+        """Adiciona os comandos ao servidor"""
+        self.cli.add_command(click.command(lambda: self.create_database()), 'create_database')
+
     def setup(self):
-        self.db = self.init_db()
+        """Inicializa o servidor"""
+        with self.app_context():
+            self.db = self.init_db()
+            self.commands()
