b3b0d048ae1299509a1f85398f6645736e33ee45
igorsaulo <igorsaulo01@gmail.com>
2023-07-09 13:29:18 -0300

    modify models

--- server/database/models/__init__.py
+++ server/database/models/__init__.py
@@ -4,5 +4,15 @@ from .espaco import Espaco
 from .espaco_de_trabalho import EspacoDeTrabalho
 from .membro import Membro
 from .membro_espaco import MembroEspaco
+from .lista import Lista
+from .mensagem import Mensagem
+from .tarefa import Tarefa
+from .subtarefa import Subtarefa
+from .permissao import Permissao
+from .grupo_permissao import GrupoPermissao
+from .membro_permissao import MembroPermissao
+from .grupo import Grupo
 
-__all__ = ['Usuario', 'Espaco', 'EspacoDeTrabalho', 'Membro', 'MembroEspaco']
+__all__ = ['Usuario', 'Espaco', 'EspacoDeTrabalho', 'Membro',
+           'MembroEspaco', 'Lista', 'Mensagem', 'Tarefa', 'Subtarefa',
+           'Permissao', 'GrupoPermissao', 'MembroPermissao', 'Grupo']
