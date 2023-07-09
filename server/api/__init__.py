4a6b7cbecd8ba66fa8c35b35aa414dd327b0dcfe
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-07-09 06:22:36 +0000

    token expirado

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,6 +1,7 @@
 """Módulo de inicialização da API"""
 from importlib import import_module
 from flask_jwt_extended.exceptions import NoAuthorizationError
+from jwt.exceptions import ExpiredSignatureError
 from flask_restx import Api
 from flask import Flask
 
@@ -11,9 +12,15 @@ api = Api()
 @api.errorhandler
 def handle_error(error):
     """Tratamento de erros da api"""
+    message = 'Erro interno do servidor'
+    code = 500
     if isinstance(error, NoAuthorizationError):
-        return {'message': 'Token não encontrado'}, 401
-    return {'message': 'Erro interno do servidor'}, 500
+        message = 'Token não encontrado'
+        code = 401
+    elif isinstance(error, ExpiredSignatureError):
+        message = 'Token expirado'
+        code = 401
+    return {'message': message}, code
 
 
 def init_app(app: Flask):
