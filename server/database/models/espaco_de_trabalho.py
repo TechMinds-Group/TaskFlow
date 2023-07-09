ece5d8a42549adfde3b83b0ef63918d92c491c0f
igorsaulo <igorsaulo01@gmail.com>
2023-07-09 02:53:27 -0300

    new models

--- server/database/models/espaco_de_trabalho.py
+++ server/database/models/espaco_de_trabalho.py
@@ -4,7 +4,6 @@ from .default import DefaultModel, db
 
 class EspacoDeTrabalho(DefaultModel):
     """Modelo do espaco de trabalho"""
-    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), nullable=False)
 
     def insert_espaco_de_trabalho(self, nome, **_):
--- server/database/models/membro.py
+++ server/database/models/membro.py
@@ -4,7 +4,6 @@ from .default import DefaultModel, db
 
 class Membro(DefaultModel):
     """Modelo do membro"""
-    id = db.Column(db.Integer, primary_key=True)
     usuario_id = db.Column(db.Integer, db.ForeignKey(
         'usuario.id'), nullable=False)
     espaco_de_trabalho_id = db.Column(
