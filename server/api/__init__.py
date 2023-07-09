e63e21b8f4a65b5b097668d74854660d8debf64f
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-07-09 03:52:40 +0000

    trantativa de autenticação e proteção de rota usuarios

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,10 +1,19 @@
 """Módulo de inicialização da API"""
 from importlib import import_module
+from flask_jwt_extended.exceptions import NoAuthorizationError
 from flask_restx import Api
 from flask import Flask
 
 
 api = Api()
+
+
+@api.errorhandler
+def handle_error(error):
+    """Tratamento de erros da api"""
+    if isinstance(error, NoAuthorizationError):
+        return {'message': 'Token não encontrado'}, 401
+    return {'message': 'Erro interno do servidor'}, 500
 
 
 def init_app(app: Flask):
